0a219ea5ca2be89bc63a2cd0be96a694b235c46c
antonosterblad <antos600@student.liu.se>
2015-03-31 10:38:19 +0200

    Button onClickListener added

--- IkeaSwipe/app/src/main/java/agilec/ikeaswipe/ArButtonFragment.java
+++ IkeaSwipe/app/src/main/java/agilec/ikeaswipe/ArButtonFragment.java
@@ -4,9 +4,11 @@ import android.app.Activity;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -14,10 +16,21 @@ import android.view.ViewGroup;
  */
 public class ArButtonFragment extends Fragment {
 
+    private Button ArButton;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ar_button, container, false);
+        View view = inflater.inflate(R.layout.fragment_ar_button, container, false);
+        ArButton = (Button) view.findViewById(R.id.Ar_button_all);
+        ArButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("ArButtonClick", "Pushed it!");
+            }
+        });
+        return view;
     }
+
 
 }
