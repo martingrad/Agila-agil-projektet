ccca9189297238713d79fa28508546f559c87c84
antonosterblad <antos600@student.liu.se>
2015-03-31 11:28:06 +0200

    Added comments to the AR fragment class

--- IkeaSwipe/app/src/main/java/agilec/ikeaswipe/ArButtonFragment.java
+++ IkeaSwipe/app/src/main/java/agilec/ikeaswipe/ArButtonFragment.java
@@ -13,18 +13,35 @@ import android.widget.Button;
 
 
 /**
-
+ * @user @ingelhag @antonosterblad
+ * Simple layout to start a new AR activity when clicking on a button.
+ * TODO design
  */
 public class ArButtonFragment extends Fragment {
 
     private Button ArButton;
 
+    /**
+     * @user @ingelhag @antonosterblad
+     * Create a view with fragment_ar_button.
+     * Sets onClickListener on the button.
+     * @param inflater
+     * @param container
+     * @param savedInstanceState
+     * @return view
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_ar_button, container, false);
         ArButton = (Button) view.findViewById(R.id.Ar_button_all);
         ArButton.setOnClickListener(new View.OnClickListener() {
+            /**
+             * @user @ingelhag @antonosterblad
+             * Start activity ArViewFindAll.
+             * @param v
+             */
+            // TODO refactor class names to define differences between activities and fragments.
             @Override
             public void onClick(View v) {
                 Log.i("ArButtonClick", "Pushed it!");
