98cd930116d127b8847c8638a53e70e53a9af30f
Anton Österblad <antos600@student.liu.se>
2015-04-14 16:28:07 +0200

    So far on edge detection, problem with loading obj files. It compile and is runnable, but is not done.

--- IkeaSwipe/app/src/main/java/agilec/ikeaswipe/ArFindAllActivity.java
+++ IkeaSwipe/app/src/main/java/agilec/ikeaswipe/ArFindAllActivity.java
@@ -19,19 +19,19 @@ import com.metaio.tools.io.AssetsManager;
  */
 public class ArFindAllActivity extends ARViewActivity {
 
-/*    *//**
+    /**
      * Rim model
-     *//*
+     */
     private IGeometry mRimModel = null;
 
-    *//**
+    /**
      * Edge visualization model
-     *//*
+     */
     private IGeometry mVizAidModel = null;
 
-    *//**
+    /**
      * Metaio SDK callback handler
-     *//*
+     */
     private MetaioSDKCallbackHandler mCallbackHandler;
 
     @Override
@@ -40,29 +40,71 @@ public class ArFindAllActivity extends ARViewActivity {
         super.onCreate(savedInstanceState);
 
         mCallbackHandler = new MetaioSDKCallbackHandler();
-    }*/
+    }
 
     @Override
-    protected int getGUILayout() {
-        return 0;
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        mCallbackHandler.delete();
+        mCallbackHandler = null;
     }
 
     @Override
     protected IMetaioSDKCallback getMetaioSDKCallbackHandler() {
-        return null;
+        mCallbackHandler = null;
+        return mCallbackHandler;
+    }
+
+    public void onButtonClick(View v)
+    {
+        finish();
+    }
+
+    public void onResetButtonClick(View v)
+    {
+        metaioSDK.sensorCommand("reset");
     }
 
     @Override
-    protected void loadContents() {
+    protected void loadContents()
+    {
+/*        mRimModel = loadModel("TutorialEdgeBasedInitialization/Assets/Custom/rim.obj");
+        mVizAidModel = loadModel("TutorialEdgeBasedInitialization/Assets/Custom/VizAid.obj");
 
+        final File envmapPath = AssetsManager.getAssetPathAsFile(getApplicationContext(), "TutorialEdgeBasedInitialization/Assets/Custom/env_map.png");
+        metaioSDK.loadEnvironmentMap(envmapPath, EENV_MAP_FORMAT.EEMF_LATLONG);
+
+        if (mRimModel != null)
+            mRimModel.setCoordinateSystemID(1);
+
+        if (mVizAidModel != null)
+            mVizAidModel.setCoordinateSystemID(2);*/
+
+        //setTrackingConfiguration("TutorialEdgeBasedInitialization/Assets/Custom/rim_tracking/Tracking.xml");
     }
 
-    @Override
-    protected void onGeometryTouched(IGeometry geometry) {
+/*    private IGeometry loadModel(final String path)
+    {
+        IGeometry geometry = null;
+        try
+        {
+            // Load model
+            final File modelPath = AssetsManager.getAssetPathAsFile(getApplicationContext(), path);
+            geometry = metaioSDK.createGeometry(modelPath);
 
-    }
+            MetaioDebug.log("Loaded geometry "+modelPath);
+        }
+        catch (Exception e)
+        {
+            MetaioDebug.log(Log.ERROR, "Error loading geometry: "+e.getMessage());
+            return geometry;
+        }
+        return geometry;
+    }*/
 
-/*    final class MetaioSDKCallbackHandler extends IMetaioSDKCallback
+
+    final class MetaioSDKCallbackHandler extends IMetaioSDKCallback
     {
 
         @Override
@@ -78,6 +120,17 @@ public class ArFindAllActivity extends ARViewActivity {
                 }
             });
         }
-    }*/
+    }
 
+    @Override
+    protected int getGUILayout()
+    {
+        return R.layout.tutorial_edge_based_initialization;
+    }
+
+    @Override
+    protected void onGeometryTouched(IGeometry geometry)
+    {
+
+    }
 }
