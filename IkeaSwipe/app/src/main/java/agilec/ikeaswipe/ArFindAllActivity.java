e85df0986c5dc1157550bbeae0045bccd424a882
Anton Österblad <antos600@student.liu.se>
2015-04-21 10:32:47 +0200

    Answered on feedback from pull request - More detailed documentation

--- IkeaSwipe/app/src/main/java/agilec/ikeaswipe/ArFindAllActivity.java
+++ IkeaSwipe/app/src/main/java/agilec/ikeaswipe/ArFindAllActivity.java
@@ -15,11 +15,12 @@ import com.metaio.sdk.jni.TrackingValues;
 import com.metaio.sdk.jni.TrackingValuesVector;
 import com.metaio.tools.io.AssetsManager;
 
-/*
-    Camera environment for Metaio
-    edge based tracking with 3D models
-    @author @antonosterblad @linneamalcherek
+/**
+ * Camera environment for Metaio
+ * Edge based tracking with 3D models
+ * Based on code from metaioSDK tutorials https://dev.metaio.com/sdk/tutorials
  */
+
 public class ArFindAllActivity extends ARViewActivity {
 
     /**
@@ -37,6 +38,11 @@ public class ArFindAllActivity extends ARViewActivity {
      */
     private MetaioSDKCallbackHandler mCallbackHandler;
 
+    /**
+     * Create instance for callbackhandler.
+     * @param savedInstanceState
+     * @author @antonosterblad @linneamalcherek
+     */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
@@ -45,6 +51,10 @@ public class ArFindAllActivity extends ARViewActivity {
         mCallbackHandler = new MetaioSDKCallbackHandler();
     }
 
+    /**
+     * Delete the callbackhandler on destroy.
+     * @author @antonosterblad @linneamalcherek
+     */
     @Override
     protected void onDestroy()
     {
@@ -53,21 +63,42 @@ public class ArFindAllActivity extends ARViewActivity {
         mCallbackHandler = null;
     }
 
+    /**
+     * getMetaioSDKCallbackHandler() is called in metaioSDKViewActivity.
+     * It is used to register the callback.
+     * @return mCallbackHandler
+     * @author @antonosterblad @linneamalcherek
+     */
     @Override
     protected IMetaioSDKCallback getMetaioSDKCallbackHandler() {
         return mCallbackHandler;
     }
 
+    /**
+     * Perform when onButtonClick.
+     * @param v View
+     * @author @antonosterblad @linneamalcherek
+     */
     public void onButtonClick(View v)
     {
         finish();
     }
 
+    /**
+     * Reset the tracking.
+     * @param v View
+     * @author @antonosterblad @linneamalcherek
+     */
     public void onResetButtonClick(View v)
     {
         metaioSDK.sensorCommand("reset");
     }
 
+    /**
+     * Set paths to which files to load.
+     * Define their local coordinate system ID.
+     * @author @antonosterblad @linneamalcherek @jacobselg
+     */
     @Override
     protected void loadContents()
     {
@@ -86,8 +117,18 @@ public class ArFindAllActivity extends ARViewActivity {
         setTrackingConfiguration("custom/rim_tracking/Tracking.xml");
     }
 
-    final class MetaioSDKCallbackHandler extends IMetaioSDKCallback {
+    /**
+     * IMetaioSDKCallback is an abstract class.
+     * Extends IMetaioSDKCallback to make a child class MetaioSDKCallbackHandler, to implement our own functions.
+     * @author @antonosterblad @linneamalcherek
+     */
+    public final class MetaioSDKCallbackHandler extends IMetaioSDKCallback {
 
+        /**
+         * runOnUiThread; runs the specified action on the UI thread.
+         * The UIThread is the main thread of execution for your application.
+         * @author @antonosterblad @linneamalcherek
+         */
         @Override
         public void onSDKReady() {
             // show GUI
@@ -101,17 +142,22 @@ public class ArFindAllActivity extends ARViewActivity {
 
         /**
          * onTrackingEvent is used to determine if an object has been identified.
+         * Doxygen: http://doxygen.metaio.com/metaioSDK40/classmetaio_1_1_i_metaio_s_d_k.html#1ae808bf2113950a6a17689c57fe9b4fe0
          * TODO: send feedback to SwipeActivity.
-         * @param trackingValuesVector
+         * @param trackingValuesVector Return pose of the tracked coordinate system.
+         * @author @martingrad @antonosterblad
          */
         @Override
+        // TrackingValuesVector get the pose of all tracked coordinate systems.
         public void onTrackingEvent(TrackingValuesVector trackingValuesVector) {
             super.onTrackingEvent(trackingValuesVector);
             {
+                // Returned tracking values may have a state Found, Tracking or Lost.
                 for (int i = 0; i < trackingValuesVector.size(); i++) {
                     final TrackingValues v = trackingValuesVector.get(i);
                     boolean foundObject = v.isTrackingState();
                     if(foundObject) {
+                        // If the object is found this is displayed on the screen with toast.
                         if(v.getCoordinateSystemID() == 1)
                         {
                             System.out.println("Object found!");
@@ -126,6 +172,7 @@ public class ArFindAllActivity extends ARViewActivity {
                             });
                         }
                     } else {
+                        // If the object is lost this is displayed on the screen with toast.
                         System.out.println("Object lost!");
                         runOnUiThread(new Runnable() {
                             @Override
@@ -147,8 +194,9 @@ public class ArFindAllActivity extends ARViewActivity {
 
     /**
      * Load 3D model
-     * @param path
+     * @param path Path to object to load
      * @return geometry
+     * @author @antonosterblad @linneamalcherek @jacobselg
      */
     private IGeometry loadModel(final String path)
     {
@@ -173,15 +221,16 @@ public class ArFindAllActivity extends ARViewActivity {
 
     /**
      * Define how to track the 3D model
-     * @param path
+     * @param path Path to which object will be used to track.
      * @return result
+     * @author @antonosterblad @linneamalcherek @jacobselg
      */
     private boolean setTrackingConfiguration(final String path)
     {
         boolean result = false;
         try
         {
-            // set tracking configuration
+            // Set tracking configuration
             final File xmlPath = AssetsManager.getAssetPathAsFile(getApplicationContext(), path);
             result = metaioSDK.setTrackingConfiguration(xmlPath);
             MetaioDebug.log("Loaded tracking configuration "+xmlPath);
@@ -194,12 +243,24 @@ public class ArFindAllActivity extends ARViewActivity {
         return result;
     }
 
+    /**
+     * Add xml layout on top of the camera view.
+     * @return activity_ar_view_find_all
+     * @author @antonosterblad @linneamalcherek
+     */
     @Override
     protected int getGUILayout()
     {
         return R.layout.activity_ar_view_find_all;
     }
 
+    /**
+     * Empty function.
+     * Have to be added when extending ARViewActivity.
+     * Else the class will get build errors.
+     * @param geometry Geometry that is touched
+     * @author @antonosterblad @linneamalcherek
+     */
     @Override
     protected void onGeometryTouched(IGeometry geometry)
     {
