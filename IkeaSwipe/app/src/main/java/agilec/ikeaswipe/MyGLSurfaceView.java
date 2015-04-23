2ac88dba631e04b90d0f67b6d8a45eb118ed7f58
Martin Gråd <martingrad@hotmail.com>
2015-04-23 17:19:23 +0200

    Started implementing different actions for different finger gestures

--- IkeaSwipe/app/src/main/java/agilec/ikeaswipe/MyGLSurfaceView.java
+++ IkeaSwipe/app/src/main/java/agilec/ikeaswipe/MyGLSurfaceView.java
@@ -3,6 +3,7 @@ package agilec.ikeaswipe;
 import android.content.Context;
 import android.opengl.GLSurfaceView;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 
 /**
  * Class MyGLSurfaceView
@@ -36,6 +37,20 @@ public class MyGLSurfaceView extends GLSurfaceView {
         // Set the Renderer for drawing on the GLSurfaceView
         setRenderer(mRenderer);
     }
+
+
+  @Override
+  public boolean onTouchEvent(MotionEvent event) {
+    int pointerCount = event.getPointerCount();
+    if(pointerCount == 2) {
+      System.out.println("Hej du klickade grafiktjosan med två fingrar!");
+    } else if(pointerCount == 1) {
+      System.out.println("Hej du klickade grafiktjosan med ett finger!");
+    } else {
+      System.out.println("Du har " + pointerCount + " fingrar!");
+    }
+    return false; //super.onTouchEvent(event);
+  }
 
   /**
    * Returns the a MyGlRenderer class
