34044ddb8fca4ec4202a0c5c88602fab97b418e7
Emma Forsling <emma.forsling.parborg@gmail.com>
2015-04-28 14:36:51 +0200

    removed unnecessary .obj and .png files

--- IkeaSwipe/app/src/main/java/agilec/ikeaswipe/MyGLRenderer.java
+++ IkeaSwipe/app/src/main/java/agilec/ikeaswipe/MyGLRenderer.java
@@ -93,6 +93,8 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
 
         gl.glTexParameterx(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MIN_FILTER, GL10.GL_LINEAR);
         gl.glTexParameterx(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MAG_FILTER, GL10.GL_LINEAR);
+
+        // To show the texture on a nexus device
         gl.glTexParameterx(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_WRAP_S, GL10.GL_CLAMP_TO_EDGE);
         gl.glTexParameterx(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_WRAP_T, GL10.GL_CLAMP_TO_EDGE);
 
