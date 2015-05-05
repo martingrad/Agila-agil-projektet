90af755a8f2cbf86c4adf302e8c500c10de3a115
Martin Gråd <martingrad@hotmail.com>
2015-05-05 11:38:55 +0200

    Implemented rotation of visualization aid model in AR activity

--- IkeaSwipe/app/src/main/java/agilec/ikeaswipe/ArFindAllActivity.java
+++ IkeaSwipe/app/src/main/java/agilec/ikeaswipe/ArFindAllActivity.java
@@ -2,10 +2,13 @@ package agilec.ikeaswipe;
 
 import java.io.File;
 
-import android.content.Context;
 import android.content.Intent;
+import android.graphics.Point;
+import android.graphics.PointF;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Display;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -14,6 +17,7 @@ import com.metaio.sdk.ARViewActivity;
 import com.metaio.sdk.MetaioDebug;
 import com.metaio.sdk.jni.IGeometry;
 import com.metaio.sdk.jni.IMetaioSDKCallback;
+import com.metaio.sdk.jni.Rotation;
 import com.metaio.sdk.jni.TrackingValues;
 import com.metaio.sdk.jni.TrackingValuesVector;
 import com.metaio.tools.io.AssetsManager;
@@ -29,6 +33,10 @@ public class ArFindAllActivity extends ARViewActivity {
   /**
    * Instance variables for 3D geometry that can be loaded within the system
    */
+
+  private float xPrev, x; // X Position
+  private float yPrev, y; // Y Position
+  private float density;  // Density for the device
 
   // 3D model
   private IGeometry mRimModel = null;
@@ -56,6 +64,7 @@ public class ArFindAllActivity extends ARViewActivity {
     Intent intent = getIntent();
     articleImgUrl = intent.getStringExtra("article");
     mCallbackHandler = new MetaioSDKCallbackHandler();
+    density = getResources().getDisplayMetrics().density;
   }
 
 
@@ -80,8 +89,8 @@ public class ArFindAllActivity extends ARViewActivity {
    */
   @Override
   protected IMetaioSDKCallback getMetaioSDKCallbackHandler(){
-          return mCallbackHandler;
-          }
+    return mCallbackHandler;
+  }
 
   /**
    * Perform when onButtonClick.
@@ -91,7 +100,7 @@ public class ArFindAllActivity extends ARViewActivity {
    */
   public void onButtonClick(View v){
           finish();
-          }
+  }
 
   /**
    * Reset the tracking.
@@ -125,7 +134,6 @@ public class ArFindAllActivity extends ARViewActivity {
 
     // Tracking.xml defines how to track the model
     setTrackingConfiguration("custom/"+articleImgUrl+"/Tracking.xml");
-
   }
 
   /**
@@ -158,7 +166,7 @@ public class ArFindAllActivity extends ARViewActivity {
      * Doxygen: http://doxygen.metaio.com/metaioSDK40/classmetaio_1_1_i_metaio_s_d_k.html#1ae808bf2113950a6a17689c57fe9b4fe0
      *
      * @param trackingValuesVector Return pose of the tracked coordinate system.
-     *                             A pose consists of a 3D translation and a 3D rotation which should act as offset.
+     * A pose consists of a 3D translation and a 3D rotation which should act as offset.
      * @author @martingrad @antonosterblad
      */
     @Override
@@ -261,7 +269,6 @@ public class ArFindAllActivity extends ARViewActivity {
     return result;
   }
 
-
   /**
    * returnToSwipeActivity starts a new SwipeActivity,
    * with information on if an object has been identified.
@@ -296,6 +303,97 @@ public class ArFindAllActivity extends ARViewActivity {
    */
   @Override
   protected void onGeometryTouched(IGeometry geometry) {
+  }
 
+  /**
+   * OnTouch is overridden in order to use swipe gestures to rotate the visualization aid model.
+   * @param v
+   * @param event
+   * @return
+   * @author @martingrad, @byggprojektedarn
+   */
+  @Override
+  public boolean onTouch(View v, MotionEvent event) {
+    setVizAidRotation(event);
+    return super.onTouch(v, event);
+  }
+
+  private boolean setVizAidRotation(MotionEvent event) {
+    // get pointer index from the event object
+    int pointerIndex = event.getActionIndex();
+
+    // get pointer ID
+    int pointerId = event.getPointerId(pointerIndex);
+
+    // get masked (not specific to a pointer) action
+    int maskedAction = event.getActionMasked();
+
+    // Set x and y
+    x = event.getX();
+    y = event.getY();
+
+    switch (maskedAction) {
+      case MotionEvent.ACTION_DOWN:
+      case MotionEvent.ACTION_POINTER_DOWN: {
+        // Make empty Point
+        PointF f = new PointF();
+        break;
+      }
+      case MotionEvent.ACTION_MOVE: { // a pointer was moved
+        rotateAndScaleModel(event.getPointerCount()); // Call and set Dx and Dy depending on the x, y, xPrev and yPrev positions
+        break;
+      }
+      case MotionEvent.ACTION_UP:
+      case MotionEvent.ACTION_POINTER_UP:
+      case MotionEvent.ACTION_CANCEL: {
+        break;
+      }
+    }
+
+    // Set prev values
+    xPrev = x;
+    yPrev = y;
+
+    return true;
+  }
+
+  /**
+   * rotateAndScaleModel rotates and scales the visualization aid model. One finger is used to
+   * rotate around the x and y axes, and two fingers are used to rotate around the z axis (using
+   * horizontal gestures) and to scale the model (using vertical gestures).
+   * @param pointerCount
+   * @author @martingrad, @byggprojektledarn
+   */
+  private void rotateAndScaleModel(int pointerCount) {
+    // Get the screen size.
+    Display display = getWindowManager().getDefaultDisplay();
+    Point size = new Point();
+    display.getSize(size);
+
+    // Initialize transformation variables.
+    float dx = 0, dy = 0, dz = 0, scale = 1.f;
+    float scaleSpeed = 4.f;
+    // TODO: density is incorrect (very small)... :S Here, the density is multiplied by 100 before
+    // use to account for this.
+    float tempDensity = density * 100.f;
+
+    // Set dx and dy depending on density and the current and previous pointer/finger positions.
+    if(pointerCount == 1) {                         // One finger -> Rotate around x and y axes.
+      dx = (x - xPrev) / tempDensity / 2.0f;
+      dy = (y - yPrev) / tempDensity / 2.0f;
+    } else if(pointerCount == 2) {                  // Two fingers -> Rotate around z axis and scale.
+      dz = (x - xPrev) / tempDensity / 2.0f;
+      // Set scale to differ from 1.0 with a small value that depends on dy (vertical swipe gesture).
+      scale = 1.f + (y - yPrev) / (tempDensity * scaleSpeed);
+    }
+
+    // Set rotation of the model.
+    // TODO: Explain this: For correct rotation, the rotation around x axis is set using -dy instead of dx, and the rotation around the y axis is set using -dx instead of dy... :S
+    mVizAidModel.setRotation(new Rotation(-dy, 0, 0), true);
+    mVizAidModel.setRotation(new Rotation(0, -dx, 0), true);
+    mVizAidModel.setRotation(new Rotation(0, 0, dz), true);
+
+    // Set scale of the model.
+    mVizAidModel.setScale(scale, true);
   }
 }
