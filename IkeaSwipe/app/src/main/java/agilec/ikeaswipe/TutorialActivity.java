a9fc117f7c380ee5dbb26fe7d2e8b9868c57e168
Hannes Ingelhag <hanin602@student.liu.se>
2015-05-12 15:57:27 +0200

    Added comments

--- IkeaSwipe/app/src/main/java/agilec/ikeaswipe/TutorialActivity.java
+++ IkeaSwipe/app/src/main/java/agilec/ikeaswipe/TutorialActivity.java
@@ -1,29 +1,20 @@
 package agilec.ikeaswipe;
 
-import java.util.Arrays;
-import java.util.Locale;
-
 import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.Window;
-import android.widget.Button;
 import android.widget.ImageButton;
 
-import org.json.JSONException;
-
-
-public class SplashActivity extends FragmentActivity {
-
+/**
+ * Author: @Ingelhag
+ */
+public class TutorialActivity extends FragmentActivity {
 
   SectionsPagerAdapter mSectionsPagerAdapter;
   SingleSwipeViewPager mViewPager;
@@ -57,7 +48,7 @@ public class SplashActivity extends FragmentActivity {
     // Set up the ViewPager with the sections adapter.
     mViewPager = (SingleSwipeViewPager) findViewById(R.id.pagerSplash);
     mViewPager.setAdapter(mSectionsPagerAdapter);
-    mViewPager.setCurrentItem(1);
+    mViewPager.setCurrentItem(1); // Start tab should be number 1
   }
 
 
@@ -68,7 +59,6 @@ public class SplashActivity extends FragmentActivity {
   public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
     public SectionsPagerAdapter(FragmentManager fm) {
-
       super(fm);
     }
 
@@ -76,8 +66,9 @@ public class SplashActivity extends FragmentActivity {
      * getItem is called to instantiate the fragment for the given page.
      * This method can be used if you want to add an activity you've created to a certain swipe view position in the app.
      *
-     * @param position Position for your swipe view, starts from 0. So position 1, will be the next window to the right.
-     * @return a PlaceholderFragment (defined as a static inner class below).
+     * @param position Position for your swipe view, starts from 1. So position 2, will be the next window to the right.
+     *                 and 0 will be the tab to the left
+     * @return the correct fragment
      * @author @marcusnygren
      */
     @Override
@@ -87,7 +78,7 @@ public class SplashActivity extends FragmentActivity {
       } else if (position == 1) {
         return new SplashScreenTwo();
       } else { // Position == 2
-        return new SplashScreenOne();
+        return new SplashScreenThree();
       }
     }
 
@@ -103,13 +94,19 @@ public class SplashActivity extends FragmentActivity {
     }
   }
 
+  /**
+   * The left tutorial fragment
+   */
   private class SplashScreenOne extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
       final View view = inflater.inflate(R.layout.splash_screen_1, container, false); // Inflate the layout for this fragment
 
+      // Get the image button
       ImageButton button = (ImageButton) view.findViewById(R.id.backToSwipe1);
+
+      // Set an actionlistener - start the SwipeActivity
       button.setOnClickListener(new View.OnClickListener() {
         public void onClick(View v) {
           System.out.println("Go to swipe from step 1");
@@ -122,14 +119,19 @@ public class SplashActivity extends FragmentActivity {
       return view;
     }
   }
-
+  /**
+   * The middle tutorial fragment
+   */
   private class SplashScreenTwo extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
       final View view = inflater.inflate(R.layout.splash_screen_2, container, false); // Inflate the layout for this fragment
 
+      // Get the image button
       ImageButton button = (ImageButton) view.findViewById(R.id.backToSwipe2);
+
+      // Set an actionlistener - start the SwipeActivity
       button.setOnClickListener(new View.OnClickListener() {
         public void onClick(View v) {
           System.out.println("Go to swipe from step 2");
@@ -142,14 +144,19 @@ public class SplashActivity extends FragmentActivity {
       return view;
     }
   }
-
+  /**
+   * The right tutorial fragment
+   */
   private class SplashScreenThree extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
       final View view = inflater.inflate(R.layout.splash_screen_3, container, false); // Inflate the layout for this fragment
 
+      // Get the image button
       ImageButton button = (ImageButton) view.findViewById(R.id.backToSwipe3);
+
+      // Set an actionlistener - start the SwipeActivity
       button.setOnClickListener(new View.OnClickListener() {
         public void onClick(View v) {
           System.out.println("Go to swipe from step 3");
